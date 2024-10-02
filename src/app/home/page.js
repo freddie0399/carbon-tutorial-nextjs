e2ae2cb26347730504cdeed6ea33b72a2abbd2c4
Freddie Hawes <freddie.hawes@ibm.com>
2024-10-02 19:40:51 +0100

    feat(tutorial): complete step 2

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -62,7 +62,6 @@ export default function LandingPage() {
                     height={498}
                   />
                 </Column>
-                ;
               </Grid>
             </TabPanel>
             <TabPanel>
